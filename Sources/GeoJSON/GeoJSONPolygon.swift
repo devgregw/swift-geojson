b4ac5df3d1e70ea11aa6e2aea07ece4b15bb7096
Greg Whatley <me@gregwhatley.dev>
2025-09-21 11:34:25 -0500

    Add polygon contains point

--- Sources/GeoJSON/GeoJSONPolygon.swift
+++ Sources/GeoJSON/GeoJSONPolygon.swift
@@ -19,6 +19,17 @@ public struct GeoJSONPolygon: Hashable, Sendable {
     public init(_ exterior: GeoJSONLinearRing) {
         self.init(exterior: exterior, holes: [])
     }
+
+    public var boundingBox: GeoJSONBoundingBox? {
+        exterior.boundingBox
+    }
+
+    public func contains(_ position: GeoJSONPosition, ignoreHoles: Bool = false) -> Bool {
+        guard exterior.boundingBox?.contains(position) == true,
+              exterior.contains(position) else { return false }
+
+        return ignoreHoles || !holes.contains { $0.contains(position) }
+    }
 }
 
 extension GeoJSONPolygon: Decodable {
@@ -30,10 +41,16 @@ extension GeoJSONPolygon: Decodable {
             throw GeoJSONDecodingError.notEnoughMembers(rings, got: 0, expected: 1)
         }
         try rings.forEach {
-            guard $0.count >= 4 else {
-                throw GeoJSONDecodingError.notEnoughMembers($0, got: $0.count, expected: 4)
+            guard $0.positions.count >= 4 else {
+                throw GeoJSONDecodingError.notEnoughMembers($0.positions, got: $0.positions.count, expected: 4)
             }
         }
         self.init(exterior: exterior, holes: Array(rings[1...]))
+    }
+}
+
+public extension Sequence where Element == GeoJSONPolygon {
+    func contains(_ position: GeoJSONPosition, ignoreHoles: Bool = false) -> Bool {
+        contains { $0.contains(position, ignoreHoles: ignoreHoles) }
     }
 }
